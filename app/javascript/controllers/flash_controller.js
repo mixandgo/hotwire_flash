8b0750eb6cf76bbf3444705d6400b6c93cf9341b
Cezar Halmagean <cezar@mixandgo.com>
2022-04-06 16:22:47 +0300

    Switched to promises

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,9 +3,30 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="flash"
 export default class extends Controller {
   connect() {
-    this.element.classList.add("animate__animated", "animate__rubberBand");
-    setTimeout(() => {
-      this.element.classList.add("animate__zoomOut");
-    }, 3000);
+    const node = this.element;
+
+    this.animateCSS("rubberBand").then(() => {
+      this.animateCSS("zoomOut").then(() => {
+        node.style.visibility = "hidden";
+      });
+    });
+  }
+
+  animateCSS(animation) {
+    // We create a Promise and return it
+    return new Promise((resolve, _reject) => {
+      const animationName = `animate__${animation}`;
+      const node = this.element;
+      node.classList.add("animate__animated", animationName);
+
+      // When the animation ends, we clean the classes and resolve the Promise
+      function handleAnimationEnd(event) {
+        event.stopPropagation();
+        node.classList.remove("animate__animated", animationName);
+        resolve("Animation ended");
+      }
+
+      node.addEventListener("animationend", handleAnimationEnd);
+    });
   }
 }
